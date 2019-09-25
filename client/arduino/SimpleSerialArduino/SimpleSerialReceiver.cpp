00eb926be2bc1c6b614949ceee3236881b9415ff
Jochem van Iterson <jochem.vaniterson@student.hku.nl>
2019-09-25 16:23:47 +0200

    Serial receiver function

--- client/arduino/SimpleSerialArduino/SimpleSerialReceiver.cpp
+++ client/arduino/SimpleSerialArduino/SimpleSerialReceiver.cpp
@@ -6,3 +6,10 @@ SimpleSerialReceiver::SimpleSerialReceiver(int startByte, int stopByte, int id){
   matchData[1] = stopByte;
   this->id = id;
 }
+bool SimpleSerialReceiver::match(byte *inBytes){
+  return inBytes[0] == matchData[0] && inBytes[1] == id && inBytes[4] == matchData[1];
+}
+long SimpleSerialReceiver::data(byte *inBytes){
+  return inBytes[2]*256+inBytes[3];
+}
+
--- client/arduino/SimpleSerialArduino/SimpleSerialReceiver.h
+++ client/arduino/SimpleSerialArduino/SimpleSerialReceiver.h
@@ -7,7 +7,11 @@ class SimpleSerialReceiver {
   public:
     SimpleSerialReceiver();
     SimpleSerialReceiver(int startByte, int stopByte, int id);
+    bool match(byte *inBytes);
+    long data(byte *inBytes);
   private:
+    byte matchData[2];
+    int id;
 };
 #endif
 
