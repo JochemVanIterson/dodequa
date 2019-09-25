81fff0829efa2c5a4b02ede34d8a211396d53489
Jochem van Iterson <jochem.vaniterson@student.hku.nl>
2019-09-25 16:00:17 +0200

    init main framework

--- server/nodejs/index.js
+++ server/nodejs/index.js
@@ -2,4 +2,18 @@
 //////////////////////////////////// Server ////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
+global.statusTotal = 2;
+global.statusIndex = 1;
 global.tools = require('./scripts/tools.js');
+
+// ---------------------------- Import libraries ---------------------------- //
+tools.statusPrinter(statusIndex++, "Loading modules");
+
+// ---------------------------------- Vars ---------------------------------- //
+tools.statusPrinter(statusIndex++, "Init Vars");
+// -------- Get arguments -------- //
+process.argv.splice(0,2);
+global.argv = require('minimist')(process.argv);
+global.debug = argv.debug || argv.d;
+
+console.log(chalk.cyan('      Setup Completed'));
