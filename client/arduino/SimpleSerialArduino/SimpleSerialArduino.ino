8b6da9dd83222f7751d70ffb0a16cea3611a0bc6
Jochem van Iterson <jochem.vaniterson@student.hku.nl>
2019-09-25 16:18:57 +0200

    Init serial bus

--- client/arduino/SimpleSerialArduino/SimpleSerialArduino.ino
+++ client/arduino/SimpleSerialArduino/SimpleSerialArduino.ino
@@ -4,7 +4,25 @@
 // -------------- PINS -------------- //
 
 // -------------- VARS -------------- //
+byte inBytes[5];
 void setup() {
+  Serial.begin(115200);
 }
+
 void loop() {
+  clearInput();
+}
+
+void getSerial(byte* inBytes){
+  // getSerial: functie om de seriële data binnen te halen en
+  // op te slaan in de variabele inBytes
+  if (Serial.available()) {
+    Serial.readBytes((char*)inBytes,4);
+    return; 
+  }
+}
+void clearInput() {
+  // clearInput: functie om de array met inkomende data leeg te maken,
+  // om te voorkomen dat de match nog een keer gelijk is.
+  memset(inBytes,0,sizeof(inBytes));
 }
