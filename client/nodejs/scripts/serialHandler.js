e993fd6cf5cc2e7a557e7133d527d19bea9a3c1d
Jochem van Iterson <jochem.vaniterson@student.hku.nl>
2019-10-06 17:26:55 +0200

    Custom input types

--- client/nodejs/scripts/serialHandler.js
+++ client/nodejs/scripts/serialHandler.js
@@ -28,40 +28,49 @@ module.exports = class {
       if(self.serialMatch(buf)){
         // Accelerometer
         if(self.serialIDMatch(buf, 100)){ //Accelerometer X
-          console.log("accx", self.serialCalc(buf));
+          console.log("accx", self.serialCalcFloat(buf));
+          appData.object.sensor.acc[0] = self.serialCalcFloat(buf);
         } else if(self.serialIDMatch(buf, 101)){ //Accelerometer Y
-          console.log("accy", self.serialCalc(buf));
+          console.log("accy", self.serialCalcFloat(buf));
+          appData.object.sensor.acc[1] = self.serialCalcFloat(buf);
         } else if(self.serialIDMatch(buf, 102)){ //Accelerometer Z
-          console.log("accz", self.serialCalc(buf));
+          console.log("accz", self.serialCalcFloat(buf));
+          appData.object.sensor.acc[2] = self.serialCalcFloat(buf);
         } else
 
         // Gyro
         if(self.serialIDMatch(buf, 110)){ //Gyro X
-          console.log("gyrx", self.serialCalc(buf));
+          console.log("gyrx", self.serialCalcFloat(buf));
+          appData.object.sensor.gyro[0] = self.serialCalcFloat(buf);
         } else if(self.serialIDMatch(buf, 111)){ //Gyro Y
-          console.log("gyry", self.serialCalc(buf));
+          console.log("gyry", self.serialCalcFloat(buf));
+          appData.object.sensor.gyro[1] = self.serialCalcFloat(buf);
         } else if(self.serialIDMatch(buf, 112)){ //Gyro Z
-          console.log("gyrz", self.serialCalc(buf));
+          console.log("gyrz", self.serialCalcFloat(buf));
+          appData.object.sensor.gyro[2] = self.serialCalcFloat(buf);
         } else
 
         // Magneto
         if(self.serialIDMatch(buf, 120)){ //Magneto X
-          console.log("magx", self.serialCalc(buf));
+          console.log("magx", self.serialCalcFloat(buf));
+          appData.object.sensor.mag[0] = self.serialserialCalcFloatCalc(buf);
         } else if(self.serialIDMatch(buf, 121)){ //Magneto Y
-          console.log("magy", self.serialCalc(buf));
+          console.log("magy", self.serialCalcFloat(buf));
+          appData.object.sensor.mag[1] = self.serialCalcFloat(buf);
         } else if(self.serialIDMatch(buf, 122)){ //Magneto Z
-          console.log("magz", self.serialCalc(buf));
+          console.log("magz", self.serialCalcFloat(buf));
+          appData.object.sensor.mag[2] = self.serialCalcFloat(buf);
         } else
 
         // Button
         if(self.serialIDMatch(buf, 130)){ //Button 1
-          console.log("button1", self.serialCalc(buf));
+          console.log("button1", self.serialCalcBool(buf));
         } else if(self.serialIDMatch(buf, 131)){ //Button 2
-          console.log("button2", self.serialCalc(buf));
+          console.log("button2", self.serialCalcBool(buf));
         } else if(self.serialIDMatch(buf, 132)){ //Button 3
-          console.log("button3", self.serialCalc(buf));
+          console.log("button3", self.serialCalcBool(buf));
         } else if(self.serialIDMatch(buf, 133)){ //Button 4
-          console.log("button4", self.serialCalc(buf));
+          console.log("button4", self.serialCalcBool(buf));
         }
       }
     }); // will have 4 bytes per data event
@@ -77,6 +86,28 @@ module.exports = class {
   serialCalc(buf){
     let bufferValues = buf.toJSON().data;
     return bufferValues[2]*256+bufferValues[3];
+  }
+  serialCalcUInt(buf){
+    let bufferValues = buf.toJSON().data;
+    let value = bufferValues[2]*256+bufferValues[3];
+    return value;
+  }
+  serialCalcInt(buf){
+    let bufferValues = buf.toJSON().data;
+    let value = bufferValues[2]*256+bufferValues[3];
+    value -= 32768;
+    return value;
+  }
+  serialCalcFloat(buf){
+    let bufferValues = buf.toJSON().data;
+    let value = bufferValues[2]*256+bufferValues[3];
+    value -= 32768;
+    value /= 100.;
+    return value;
+  }
+  serialCalcBool(buf){
+    let bufferValues = buf.toJSON().data;
+    return bufferValues[3]!=0;
   }
   serialWrite(id, data){
     var buf = new Buffer.alloc(5);
